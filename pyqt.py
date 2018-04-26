0c2aef880cfa303681585b8f2aa72563a602c1f7
zoe <zgardner@fwparker.org>
2018-04-25 20:35:39 -0500

    list notes added

--- pyqt.py
+++ pyqt.py
@@ -34,8 +34,38 @@ class Window(QWidget):
         # slot is a place for the signal to go
 
         btn1.clicked.connect(lambda: label_btn1.setText("Pushed"))
+
+        lcd = QLCDNumber(self)
+        lcd.display(50)
+        grid.addWidget(lcd, 4, 1, 1, 2)
+
+        sld = QSlider(Qt.Horizontal, self)
+        sld.setValue(50)
+        grid.addWidget(sld, 5, 1, 1, 2)
+
+        self.combobox = QComboBox(self)
+        self.combobox.addItems(["Zoe", "Grace", "Mathew"])
+        grid.addWidget(self.combobox, 6, 1, 1, 1)
+
+        checkbox = QCheckBox("Check me!", self)
+        grid.addWidget(checkbox, 6, 2, 1, 1)
+
+        lineedit = QLineEdit(self)
+        grid.addWidget(lineedit, 7, 1, 1, 1) # single line
+
+        textedit = QTextEdit(self)
+        grid.addWidget(textedit, 7, 2, 1, 1) # multi line
+
+        calendar = QCalendarWidget(self)
+        grid.addWidget(calendar, 8, 1, 1, 2)
+
+        sld.valueChanged.connect(lcd.display)  # true signal to slot
+        self.combobox.currentTextChanged.connect(self.combo_print)
+
         # draw out gui
         self.show()
+    def combo_print(self):
+        print("My favorite is", self.combobox.currentText())
 
 if __name__ == "__main__":
     ''' main program goes here'''
